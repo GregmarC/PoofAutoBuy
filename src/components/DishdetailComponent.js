8d06a6dbc737b28175b50c14ad3fa6f68babc601
Gregmar Cruz <gregr1201@gmail.com>
2019-12-02 13:58:13 -0500

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { addComment } from '../redux/ActionCreators';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = val => val && val.length;
@@ -94,7 +95,7 @@ class CommentForm extends Component {
             return(
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
